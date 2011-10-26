5a801d74c8bdb7f056e327c668267984f5c676b7
mariano@versalitas.com.ar <mariano@versalitas.com.ar@78482921-3c34-3bef-0630-109610e06881>
2011-10-26 13:48:47 +0000

    different strategy for sitemaps

--- trunk/versapp/__init__.py
+++ trunk/versapp/__init__.py
@@ -52,7 +52,6 @@ def initialize(template_path, *args, **kwargs):
 	""" 
 	jinja2_globals = {
 		'uri_for': webapp2.uri_for,
-		'sitemap_entries': sitemap_entries,
 	}
 	jinja2_globals.update(kwargs.pop('globals', {}))
 	jinja2e.default_config = {
@@ -87,8 +86,8 @@ class Route(webapp2.Route):
 		super(Route, self).__init__(template, handler=handler, name=name, build_only=kw.pop("build_only", None), defaults=kw.pop("defaults", None), methods=kw.pop("methods", ('GET', 'HEAD')), schemes=kw.pop("schemes", None))
 		self.cache_control = kw.pop("cache_control", None)
 		self.content_type = kw.pop("content_type", "text/html")
-		self.in_sitemap = kw.pop("in_sitemap", False)
 		self.sitemap_args = kw.pop("sitemap_args", None)
+		self.sitemap_group = kw.pop("sitemap_group", DEFAULT_SITEMAP_GROUP if kw.pop("in_sitemap", False) else None)
 		self.defaults.update(kw)
 	#
 	def build_response(self, body=None, unicode_body=None):
@@ -113,7 +112,7 @@ class Route(webapp2.Route):
 		elif callable(self.sitemap_args):
 			return self.sitemap_args()
 		else:
-			return self.sitemap_args
+			return self.sitemap_args # @ToDo clone the map so it is not modified
 	#
 	def build(self, request, args, kwargs):
 		if kwargs.pop('_canonical', False):
@@ -180,14 +179,30 @@ class RequestHandler(webapp2.RequestHandler):
 		m_a['Language'] = request.environ['REQUEST_LANGUAGE']
 		m_a['route_name'] = request.route.name
 		self.mapping_args = m_a
+	#
+	def check_last_modified(self, response):
+		prev_etag = self.request.get("_rebuild_sitemap", default_value=None)
+		if not prev_etag:
+			return
+		if prev_etag != response.etag:
+			# store new etag and last-modified
+			logging.error("modified %s" % self.request.path)
+			SitemapEntries.update(group=self.request.route.sitemap_group, loc=self.request.path_url, etag=response.etag, priority="0.4")
+		# ToDo how to customize priority and changefreq
+			
 #
 class TemplateHandler(RequestHandler):
+	def head(self, *args, **kwargs):
+		response = self.get(*args, **kwargs)
+		# we do this only in HEAD requests
+		self.check_last_modified(response)
 	def get_file(self):
 		if hasattr(self.request.route, 'template_format'):
 			return self.request.route.template_format % self.mapping_args
 		else:
 			return self.request.route.template_file
 	def build_mapping(self):
+		"""This method builds the argument map to render the template. Override to customize the map"""
 		return self.mapping_args
 	def get(self, *args, **kwargs):
 		template_file = self.get_file()
@@ -210,6 +225,8 @@ class TemplateHandler(RequestHandler):
 #
 #
 class StaticHandler(RequestHandler):
+	def head(self, *args, **kwargs):
+		self.get(*args, **kwargs)
 	def get(self, *args, **kwargs):
 		if self.get_default('versioned'):
 			versioned_arg = self.get_default('versioned')
@@ -227,7 +244,92 @@ class StaticHandler(RequestHandler):
 			content = read_binary_file(self.request.route.template_file)
 		r = self.request.route.build_response(body=content)
 		return r
-#	
+#
+DEFAULT_SITEMAP_GROUP = "default"
+class SitemapEntry(object):
+	def __init__(self, loc, lastmod=None, image_loc=None, priority=None, changefreq=None):
+		self.loc = loc
+		self.priority = priority
+		self.lastmod = lastmod
+		self.image_loc = image_loc
+		self.changefreq = changefreq
+#
+class SitemapEntries(db.Model):
+	group = db.StringProperty()
+	loc = db.StringProperty()
+	etag = db.StringProperty()
+	lastmod = db.DateProperty(auto_now=True)
+	priority = db.StringProperty()
+	changefreq = db.StringProperty()
+	image_loc = db.StringProperty()
+	#
+	@classmethod
+	def get_entries(cls, group):
+		# ToDo (mariano): implement caching
+		result = []
+		for r in db.Query(cls).filter("group =", group):
+			result.append(r)
+		return result
+	@classmethod
+	def update(cls, group, loc, etag, priority=None, changefreq=None, image_loc=None):
+		SitemapEntries(key_name=loc, group=group, loc=loc, etag=etag, priority=priority, changefreq=changefreq, image_loc=image_loc).put()
+#
+class SitemapHandler(RequestHandler):
+	"""We support multiple sitemaps in the application by using the sitemap_group attribute when defining the routes using this handler.
+	
+	
+	"""
+	#
+	#
+	def initialize(self, request, response):
+		super(SitemapHandler, self).initialize(request, response)
+		self.sitemap_group = self.get_default('sitemap_group', DEFAULT_SITEMAP_GROUP)
+	#
+	def router_urls(self):
+		"""Traverses all the routes and retrieves the possible urls for each one.
+			Returns: a list of tupes (canonical, path)
+		"""
+		result = [] 
+		#
+		for r in self.app.router.match_routes:
+			if r.sitemap_group != self.sitemap_group:
+				continue
+			for args, kwargs, priority in r.get_sitemap_args():
+				 # @ToDo sitemap_args should be inmutable
+				path = r.build(self.request, args, dict(kwargs))
+				kwargs = dict(kwargs)
+				kwargs['_canonical'] = True
+				canonical = r.build(self.request, args, kwargs)
+				result.append((canonical, path))
+		return result
+	#
+	def defer_rebuild(self, router_urls, current_entries):
+		from google.appengine.api import taskqueue
+		entries_map = dict(map(lambda e: (e.loc, e.etag), current_entries))
+		for canonical, path in router_urls:
+			etag = entries_map.get(canonical, 'New')
+			taskqueue.Queue("default").add(taskqueue.Task(url=path, params={'_rebuild_sitemap': etag}, method='HEAD'))
+	#	
+	def get(self, *args, **kwargs):
+		entries = self.build_entries()
+		if self.request.get('_rebuild'):
+			urls = self.router_urls()
+			self.defer_rebuild(urls, entries)
+		#
+		template_file = self.request.route.template_file
+		content = self.render_template(template_file, entries=entries)
+		r = self.request.route.build_response(unicode_body=content)
+		return r
+	#
+	def build_entries(self):
+		"""
+			returns the list of entries to include in this sitemap.
+			This method is here to allow a simple inheritance so you can customize the way you obtain the entries.
+		"""
+		return SitemapEntries.get_entries(self.sitemap_group)
+	
+		
+#
 def new_route(handler, template, name, **kw):
 	defaults = kw.setdefault('defaults', {})
 	if kw.get('versioned_arg'):
